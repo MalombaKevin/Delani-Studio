55e73259286b212f104d26c352031a78f7b5d693
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-03-13 21:43:37 +0300

    feat-jquery image plugin: adds hover effect on overlya text using class snake

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 $(Document).ready(function () {
-
 	// click to toggle
 	$('.imageDesign').click(function () {
 		$('.textDesign').toggle();
@@ -15,16 +14,23 @@ $(Document).ready(function () {
 	});
 
 	// hover to pop up project name
-
-
+	
+		$('.snake').snakeify({
+			speed: 130
+		});
+	
 
 	// form data and alert message to user sending message
 
 	$('.contact form').submit(function (event) {
 		var contactName = $('#name').val();
 		var contactEmail = $('#email').val();
-		var contactMessage=$('#message').val();
-	alert("Hi " + contactName +". Thank you for contacting Delani Studio.We will get back to you as soon as possible.")
+		var contactMessage = $('#message').val();
+		alert(
+			'Hi ' +
+				contactName +
+				'. Thank you for contacting Delani Studio. We will get back to you as soon as possible.'
+		);
 
 		event.preventDefault();
 	});
