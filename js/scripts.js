dbf1e887c7e49870762eae4761c74eb9c9550187
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-03-13 12:48:40 +0300

    adds toggle feature: switch between icon and text image

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-!(Document).ready(function(){
+$(Document).ready(function(){
+
+$('.imageDesign').hover(function(){
+    $(".textDesign").toggle();
+    $(".imageDesign").toggle();   
+});
+$('.imageDev').click(function(){
+    $(".textDev").toggle();
+    $(".imageDev").toggle();   
+});
+$('.imageProdman').click(function(){
+    $(".textProdman").toggle();
+    $(".imageProdman").toggle();   
+});
 
 });
