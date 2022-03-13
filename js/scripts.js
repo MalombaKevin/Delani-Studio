748430a929419aae78eeba3f80d55af3dedb0398
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-03-13 16:38:08 +0300

    feat:dom manipulation: collect data filled and alert user

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,31 @@
-$(Document).ready(function(){
+$(Document).ready(function () {
 
-$('.imageDesign').hover(function(){
-    $(".textDesign").toggle();
-    $(".imageDesign").toggle();   
-});
-$('.imageDev').click(function(){
-    $(".textDev").toggle();
-    $(".imageDev").toggle();   
-});
-$('.imageProdman').click(function(){
-    $(".textProdman").toggle();
-    $(".imageProdman").toggle();   
-});
+	// click to toggle
+	$('.imageDesign').click(function () {
+		$('.textDesign').toggle();
+		$('.imageDesign').toggle();
+	});
+	$('.imageDev').click(function () {
+		$('.textDev').toggle();
+		$('.imageDev').toggle();
+	});
+	$('.imageProdman').click(function () {
+		$('.textProdman').toggle();
+		$('.imageProdman').toggle();
+	});
 
+	// hover to pop up project name
+
+
+
+	// form data and alert message to user sending message
+
+	$('.contact form').submit(function (event) {
+		var contactName = $('#name').val();
+		var contactEmail = $('#email').val();
+		var contactMessage=$('#message').val();
+	alert("Hi " + contactName +". Thank you for contacting Delani Studio.We will get back to you as soon as possible.")
+
+		event.preventDefault();
+	});
 });
